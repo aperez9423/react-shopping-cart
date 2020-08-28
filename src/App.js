161f1b491f375948ebfb45ed8e2eb4015a54c754
Angelica Perez <aperez9423@gmail.com>
2020-08-27 20:44:03 -0400

    met MVP requirements

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { CartContext } from './contexts/CartContext';
+import { ProductContext } from './contexts/ProductContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,20 +15,26 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		return (
+			setCart([...cart, item])
+		)
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
+			<ProductContext.Provider value = {{ products, addItem }}>
+				<CartContext.Provider value = {cart}>
+					<Navigation cart={cart} />
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+					<Route exact path="/">
+						<Products products={products} addItem={addItem} />
+					</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
